23c6b38e8d82102732b29225f470a8e55b72e783
Alexander Heimbuch <github@heimbu.ch>
2018-10-03 09:17:10 +0200

    chore(build): adds umd build for better compatibility

--- index.js
+++
@@ -1,5 +0,0 @@
-import { connect } from './src/mixin'
-import mapState from './src/map-state'
-import mapActions from './src/map-actions'
-
-export { connect, mapState, mapActions }
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'redux-vuex.js',
+    library: 'redux-vuex',
+    libraryTarget: 'umd'
+  }
+}
